4a96bb284ea9014402c8634d8650e1995d4df5f7
kevin-steve772 <kevin202634@outlook.com>
2026-06-06 05:51:50 +0000

    汉化了测试程序

--- test.cpp
+++ test.cpp
@@ -4,13 +4,9 @@
 int main() {
     int w = termw();
     int h = termh();
-    std::cout << "Terminal width: " << w << ", height: " << h << std::endl;
-
-    clrtxt(std::string("Hello in red on default background\n"), RED, BG_DEFAULT);
-
-    // Move cursor to 1, h to show mvc works
+    std::cout << "终端信息（测试termw()和termh()的功能） - 终端宽度: " << w << ", 高度: " << h << std::endl;
+    clrtxt(std::string("测试文本（测试clrtxt()的功能） - 红色文本，默认背景\n"), RED, BG_DEFAULT);
     mvc(1, h);
-    clrtxt(std::string("Bottom-left text (green)\n"), GREEN, BG_DEFAULT);
-
+    clrtxt(std::string("测试文本（测试mvc()和clrtxt()的功能） - 底部，绿色文本，默认背景\n"), GREEN, BG_DEFAULT);
     return 0;
 }
